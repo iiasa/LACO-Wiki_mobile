694ed8198d813c5e7f1c1126de32dafb91a4bab0
LIGEARD Frederic <fligeard@IDSI0.si.c-s.fr>
2018-11-23 08:56:42 +0100

    save

--- src/LacoWikiMobile.App/Core/Api/ApiClient.cs
+++ src/LacoWikiMobile.App/Core/Api/ApiClient.cs
@@ -19,7 +19,7 @@ namespace LacoWikiMobile.App.Core.Api
 			ApiAuthentication = apiAuthentication;
 		}
 
-		protected string BaseUrl => "https://dev.laco-wiki.net/api/mobile";// //https://laco-wiki.net/api/mobile
+		protected string BaseUrl => "https://dev.laco-wiki.net/api/mobile"; // //https://laco-wiki.net/api/mobile
 		protected string BaseCacheUrl => "https://tilecache.dev.geo-wiki.org/v1/";
 
 		protected IApiAuthentication ApiAuthentication { get; set; }
